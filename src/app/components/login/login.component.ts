42cb200c2f828ade963eb4aced385441079eb455
MateuszR <mateusz.rysula@gmail.com>
2024-04-26 20:25:24 +0200

    added basic view for login component, generated register and main templates

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    MatSlideToggle
+    MatSlideToggle,
+    MatCardContent,
+    MatError,
+    MatCard,
+    FormsModule,
+    NgIf,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatLabel
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 export class LoginComponent {
+  public loginValid = true;
+
+  public onSubmit(): void {
+    console.log('Form submitted');
+  }
 
 }
