c9552b79734a2d41974a559c85c2e202d0512f29
MateuszR <mateusz.rysula@gmail.com>
2024-05-09 18:53:58 +0200

    Cleanup

--- src/app/components/all-books/all-books.component.ts
+++ src/app/components/all-books/all-books.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import {BookDTO, DefaultService} from "../../openapi";
-import {Observable, of} from "rxjs";
-import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
-import {Router} from "@angular/router";
-import {MatSliderChange} from "@angular/material/slider";
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import { Router } from "@angular/router";
+import { Observable, of } from "rxjs";
+import { BookDTO, DefaultService } from "../../openapi";
 
 @Component({
   selector: 'app-all-books',
@@ -11,25 +10,56 @@ import {MatSliderChange} from "@angular/material/slider";
   styleUrls: ['./all-books.component.css']
 })
 export class AllBooksComponent {
-  books: BookDTO[] = [];
-  bookCovers: Map<string, Blob> = new Map();
-  filteredBooks: BookDTO[] = [];
-  searchTerm: string = '';
-  alphabeticalOrder: boolean = false;
-  ratingFilter: number = 1;
-  ratingFilterEnabled: boolean = false;
-  originalBooksOrder: BookDTO[] = [];
+  private books: BookDTO[] = [];
+  private originalBooksOrder: BookDTO[] = [];
+  private bookCovers: Map<string, Blob> = new Map();
+
+  public filteredBooks: BookDTO[] = [];
+  public searchTerm: string = '';
+  public alphabeticalOrder: boolean = false;
+  public ratingFilter: number = 1;
+  public ratingFilterEnabled: boolean = false;
 
   constructor(private api: DefaultService, private sanitizer: DomSanitizer, private router: Router) { }
 
   ngOnInit(): void {
+    this.fetchAllBooks();
+  }
+
+
+  navigateToBookDetails(isbn: string | undefined): void {
+    if (isbn) {
+      this.router.navigate(['/book-details', isbn]).then(r => console.log('Navigation result:', r));
+    }
+  }
+
+
+  searchBooks(): void {
+    let filteredBooks = this.books;
+
+    if (this.searchTerm) {
+      filteredBooks = this.filterBySearchTerm(filteredBooks);
+    }
+
+    if (this.alphabeticalOrder) {
+      filteredBooks = this.sortAlphabetically(filteredBooks);
+    } else {
+      filteredBooks = [...this.originalBooksOrder];
+    }
+
+    if (this.ratingFilterEnabled) {
+      filteredBooks = this.filterAndSortByRating(filteredBooks);
+    }
+
+    this.filteredBooks = filteredBooks;
+  }
+
+  private fetchAllBooks(): void {
     this.api.getAllBooks().subscribe({
       next: (books) => {
         this.books = books;
-        this.originalBooksOrder = [...books]; // dodane
-        this.books.forEach(book => {
-          this.fetchBookCover(book.coverName, book.isbn);
-        });
+        this.originalBooksOrder = [...books];
+        this.fetchBookCovers(books);
         this.searchBooks();
       },
       error: error => {
@@ -38,27 +68,22 @@ export class AllBooksComponent {
     });
   }
 
-  formatLabel(value: number): string {
-    if (value >= 1000) {
-      return Math.round(value / 1000) + 'k';
-    }
-
-    return `${value}`;
-  }
-
-  private fetchBookCover(coverName?: string, isbn?: string): void {
-    if (coverName && isbn) {
-      this.api.getBookCover(coverName).subscribe({
-        next: (cover) => {
-          this.bookCovers.set(isbn, cover);
-        },
-        error: error => {
-          console.error('Error during fetching cover:', error);
-        }
-      });
-    } else {
-      console.error('Cover name or book id is undefined');
-    }
+  private fetchBookCovers(books: BookDTO[]): void {
+    books.forEach(book => {
+      if (book.coverName && book.isbn) {
+        this.api.getBookCover(book.coverName).subscribe({
+          next: (cover) => {
+            if (book.isbn)
+            this.bookCovers.set(book.isbn, cover);
+          },
+          error: error => {
+            console.error('Error during fetching cover:', error);
+          }
+        });
+      } else {
+        console.error('Cover name or book id is undefined');
+      }
+    });
   }
 
   getCoverUrl(isbn: string | undefined): Observable<SafeUrl | undefined> {
@@ -72,53 +97,31 @@ export class AllBooksComponent {
     return of(undefined);
   }
 
-  navigateToBookDetails(isbn: string | undefined): void {
-    if (isbn) {
-      this.router.navigate(['/book-details', isbn]).then(r => console.log('Navigation result:', r));
-    }
+  private filterBySearchTerm(books: BookDTO[]): BookDTO[] {
+    return books.filter(book => book.title ? book.title.toLowerCase().includes(this.searchTerm.toLowerCase()) : false);
   }
 
-  updateRatingFilter(event: MatSliderChange): void {
-    this.ratingFilter = event.value;
-    this.searchBooks();
+  private sortAlphabetically(books: BookDTO[]): BookDTO[] {
+    return books.sort((a, b) => {
+      if (a.title && b.title) {
+        const titleA = a.title.toLowerCase().replace(/\s/g, '');
+        const titleB = b.title.toLowerCase().replace(/\s/g, '');
+        return titleA.localeCompare(titleB, 'pl');
+      } else {
+        return 0;
+      }
+    });
   }
 
-  searchBooks(): void {
-    console.log('searchBooks called');
-    console.log('ratingFilterEnabled:', this.ratingFilterEnabled);
-
-    let filteredBooks = this.books;
-
-    if (this.searchTerm) {
-      filteredBooks = filteredBooks.filter(book => book.title ? book.title.toLowerCase().includes(this.searchTerm.toLowerCase()) : false);
-    }
-
-    if (this.alphabeticalOrder) {
-      filteredBooks = filteredBooks.sort((a, b) => {
-        if (a.title && b.title) {
-          const titleA = a.title.toLowerCase().replace(/\s/g, '');
-          const titleB = b.title.toLowerCase().replace(/\s/g, '');
-          return titleA.localeCompare(titleB, 'pl');
+  private filterAndSortByRating(books: BookDTO[]): BookDTO[] {
+    return books
+      .filter(book => book.averageRating ? book.averageRating >= this.ratingFilter : false)
+      .sort((a, b) => {
+        if (typeof a.averageRating === 'number' && typeof b.averageRating === 'number') {
+          return b.averageRating - a.averageRating;
         } else {
           return 0;
         }
       });
-    } else {
-      filteredBooks = [...this.originalBooksOrder]; // Przywrócenie oryginalnej kolejności
-    }
-
-    if (this.ratingFilterEnabled) {
-      filteredBooks = filteredBooks
-        .filter(book => book.averageRating ? book.averageRating >= this.ratingFilter : false)
-        .sort((a, b) => {
-          if (typeof a.averageRating === 'number' && typeof b.averageRating === 'number') {
-            return b.averageRating - a.averageRating;
-          } else {
-            return 0;
-          }
-        });
-    }
-
-    this.filteredBooks = filteredBooks;
   }
 }
